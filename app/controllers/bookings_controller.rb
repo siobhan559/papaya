438e500811d1cd0479cf52757d912a2d1198e2e2
Cesar Ades <kito@ades.as>
2021-08-31 14:42:51 +0100

    booking actions filled in

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new
+    @event = Event.find(params[:event_id])
+    @booking.user = current_user
+    @booking.event = @event
+    if @booking.save
+      redirect_to event_path(@event)
+    else
+      #
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    # reder back
+  end
 end
