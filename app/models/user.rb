e25c5185dda534073b062f7d41de744413013d4d
Cesar Ades <kito@ades.as>
2021-08-31 14:19:28 +0100

    authetication pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   # This is the events they registered for
   has_many :events, through: :bookings, as: :registered_events
   # This is the events the organization creates
-  has_many :events, as: :created_events
+  has_many :events, as: :created_events, dependent: :destroy
 end
