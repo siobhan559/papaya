823e15a1966c5d8b5616d54ef81dffe6b827207e
Cesar Ades <kito@ades.as>
2021-09-06 11:02:49 +0100

    Working messaging

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,13 @@ class Chatroom < ApplicationRecord
   has_many :messages
   belongs_to :owner, class_name: "User"
   belongs_to :invitee, class_name: "User"
+  validates :owner, :invitee, presence: true
+
+  def exist_with?(user1, user2)
+    (owner == user1 || invitee == user1) && (owner == user2 || invitee == user2)
+  end
+
+  def other(current)
+    owner == current ? invitee.name : owner.name
+  end
 end
