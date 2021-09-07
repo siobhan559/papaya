7748e506c48412b5668d3211dd029263a62f992e
Cesar Ades <kito@ades.as>
2021-09-07 17:38:08 +0100

    Styled chatroom

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -3,6 +3,7 @@ import consumer from "./consumer";
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
+    // messagesContainer.scrollTo(document.getElementById('bottom'));
     const id = messagesContainer.dataset.chatroomId;
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
