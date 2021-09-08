7cdafe169734a336d02904015eb3ff1a9cfe448b
Cesar Ades <kito@ades.as>
2021-09-08 18:28:51 +0100

    Bullet complete for profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,13 +19,13 @@ class PagesController < ApplicationController
     if @profile == current_user
       if @profile.organization
         # current user's profile page for organization
-        @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
-        @past_organized = @profile.events.select { |event| event.start_time < DateTime.now }
+        @upcoming_organized = @profile.events.includes([photo_attachment: :blob]).select { |event| event.start_time > DateTime.now }
+        @past_organized = @profile.events.includes([photo_attachment: :blob]).select { |event| event.start_time < DateTime.now }
         @new_event = Event.new
       else
         # current user's profile page for volunteer
-        @upcoming_registered = @profile.registered_events.select { |event| event.start_time > DateTime.now }
-        @past_registered = @profile.registered_events.select { |event| event.start_time < DateTime.now }
+        @upcoming_registered = @profile.registered_events.includes([user: { photo_attachment: :blob }, photo_attachment: :blob]).select { |event| event.start_time > DateTime.now }
+        @past_registered = @profile.registered_events.includes([user: { photo_attachment: :blob }, photo_attachment: :blob]).select { |event| event.start_time < DateTime.now }
         @favorites = @profile.all_favorited
         @followings = @profile.following
       end
@@ -33,11 +33,11 @@ class PagesController < ApplicationController
       @chatroom = Chatroom.includes(%i[owner invitee]).find { |chatroom| chatroom.exist_with?(@profile, current_user) }
       if @profile.organization && current_user.organization
         # visiting an organization's profile page as an organization
-        @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
+        @upcoming_organized = @profile.events.includes([photo_attachment: :blob]).select { |event| event.start_time > DateTime.now }
       elsif @profile.organization
         # visiting an organization's profile page as a volunteer
         @am_following = current_user.following.find { |user| user == @profile }.present?
-        @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
+        @upcoming_organized = @profile.events.includes([photo_attachment: :blob]).select { |event| event.start_time > DateTime.now }
       end
       # visiting a volunteer's profile page
     end
