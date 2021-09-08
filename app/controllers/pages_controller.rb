50ecda576ef4d9de5a88e75a6f0a614278257a88
Cesar Ades <kito@ades.as>
2021-09-08 16:00:49 +0100

    bullet show profile organization

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,11 +10,14 @@ class PagesController < ApplicationController
   def profile
     @profile = User.find(params[:id])
     if @profile == current_user
-      @upcoming = @profile.registered_events.select { |event| event.start_time > DateTime.now }
-      @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
-      @past = @profile.registered_events.select { |event| event.start_time < DateTime.now }
-      @past_organized = @profile.events.select { |event| event.start_time < DateTime.now }
-      @event = Event.new if @profile.organization
+      if @profile.organization
+        @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
+        @past_organized = @profile.events.select { |event| event.start_time < DateTime.now }
+        @event = Event.new
+      else
+        @upcoming = @profile.registered_events.select { |event| event.start_time > DateTime.now }
+        @past = @profile.registered_events.select { |event| event.start_time < DateTime.now }
+      end
     else
       @chatroom = Chatroom.includes(%i[owner invitee]).find { |chatroom| chatroom.exist_with?(@profile, current_user) }
       @following = current_user.following.find { |user| user == @profile }.present?
