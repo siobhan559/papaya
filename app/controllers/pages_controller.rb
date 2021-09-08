7d77e4e116f153db76817f6b0c2fa8d49a29bb74
Siobhan <siobhanbrown559@gmail.com>
2021-09-08 10:07:18 +0100

    organization events showing on profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,9 @@ class PagesController < ApplicationController
     @profile = User.find(params[:id])
     if @profile == current_user
       @upcoming = @profile.registered_events.select { |event| event.start_time > DateTime.now }
+      @upcoming_organized = @profile.events.select { |event| event.start_time > DateTime.now }
       @past = @profile.registered_events.select { |event| event.start_time < DateTime.now }
+      @past_organized = @profile.events.select { |event| event.start_time < DateTime.now }
       @event = Event.new if @profile.organization
     else
       @chatroom = Chatroom.all.find { |chatroom| chatroom.exist_with?(@profile, current_user) }
