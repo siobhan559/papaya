c23828e5ec30e398d4f72f03f8d04a9c46bb54a6
Siobhan <siobhanbrown559@gmail.com>
2021-09-09 10:18:36 +0100

    mapbox zoom duration shorter

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,7 +7,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 800 });
   };
 
 
@@ -23,7 +23,7 @@ const initMapbox = () => {
     markers.forEach((marker) => {
       const element = document.createElement('div');
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      element.className = 'marker'; 
+      element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
       element.style.backgroundSize = 'contain';
       element.style.width = '25px';
